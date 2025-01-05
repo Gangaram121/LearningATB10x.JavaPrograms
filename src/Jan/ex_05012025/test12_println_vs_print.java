fd57a00f43a158ec6476c1a701b4f6040e05810c
Rambabu <rambabu.ganga01@gmail.com>
2025-01-05 17:51:52 +0530

    Newchangestest12

---
+++ src/Jan/ex_05012025/test12_println_vs_print.java
@@ -0,0 +1,8 @@
+package Jan.ex_05012025;
+
+public class test12_println_vs_print {
+    public static void main(String[] args) {
+        System.out.println("Testing123");
+        System.out.println("Testingram098");
+    }
+}
